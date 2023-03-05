423753d59bf2f718a4c8052f167f06bd6e423834
bbaranoff <bastienbaranoff@gmail.com>
2023-03-06 00:11:24 +0100

    commit

--- v7-services/src/networking/day.ts
+++
@@ -1,5 +0,0 @@
-export function getDayOrigin() {
-    const date = new Date();
-    return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
-        Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000 + date.getFullYear() * 366;
-}
--- v7-services/src/ok.ts
+++
@@ -1,7 +0,0 @@
-import { Handler } from "aws-lambda";
-
-import { success } from "./responses-proxy";
-
-export const ok: Handler = async (event: any) => {
-    return success({});
-};
